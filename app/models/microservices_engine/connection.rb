2bacf75bce889f215d59f6553d67bf94e5378c9b
anbranin <keichelman@gmail.com>
2017-02-20 12:40:41 -0500

    rename module

--- app/models/microservices_engine/connection.rb
+++ app/models/microservices_engine/connection.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'net/http'
 
-module MicroservicesEngine
+module MicroservicesEngineRails3
   class Connection < ActiveRecord::Base
     validates :url, :object, presence: true
     attr_accessible :name, :url, :object
