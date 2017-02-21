3bdf9c673de2f2f4e2ae805b155e6a6cbbf911e0
anbranin <keichelman@gmail.com>
2017-02-21 11:08:57 -0500

    name change

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-MicroservicesEngine::Engine.routes.draw do
+MicroservicesEngineRails3::Engine.routes.draw do
   namespace :v1 do
     match '/data' => 'data#register', :via => :post
   end
